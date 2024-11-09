2389d593c4d64024cb740581b9517a44ae0f4f4a
Rowan Fahy <rowanfahy@outlook.com>
2024-11-09 22:48:32 +0000

    Added comments and cleaned up formatting in areas

--- gol/distributor.go
+++ gol/distributor.go
@@ -9,6 +9,14 @@ import (
 	"uk.ac.bris.cs/gameoflife/util"
 )
 
+/*
+All tests that we passed for convenience
+
+go test -v -run TestGol/-1$
+go test -v -run TestAlive
+go test -v -run TestPgm/-1$
+*/
+
 type distributorChannels struct {
 	events     chan<- Event
 	ioCommand  chan<- ioCommand
@@ -18,17 +26,20 @@ type distributorChannels struct {
 	ioInput    <-chan uint8
 }
 
+//Response struct to hold information needed from the server's Game of Life response
 type Response struct {
 	FinalWorld   [][]byte
 	AliveCells   []util.Cell
 	TurnsElapsed int
 }
 
+//Request struct to hold information that we need to send to the server
 type GolRequest struct {
 	Params Params
 	World  [][]byte
 }
 
+//Response struct for getting aliveCells back when we use the ticker
 type AliveCellsResponse struct {
 	NumAliveCells int
 	TurnsElapsed int
@@ -36,16 +47,23 @@ type AliveCellsResponse struct {
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
-	quit := make(chan bool)
-	c.ioCommand <- ioInput
-	c.ioFilename <- (strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight))
 
+	//Create channel for safely quitting goroutines
+	quit := make(chan bool)
+
+	//Give io the command to read an input file via ioCommand channel, then use ioFilename along with the width and height to find the correct input file
+	filename := (strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight))
+	c.ioCommand <- ioInput
+	c.ioFilename <- filename
+
+	//Initialise World
 	fmt.Println("Creating slice for world")
-	// TODO: Create a 2D slice to store the world.
 	world := make([][]byte, p.ImageHeight)
 	for i := range world {
 		world[i] = make([]byte, p.ImageWidth)
 	}
+
+	//Use the ioInput channel to receive the data from the input file and write it into world
 	fmt.Println("Populating world")
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
@@ -53,12 +71,16 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}
 
+	//Initialise our request for the server
 	request := GolRequest{p, world}
 
-	turn := 0
-	c.events <- StateChange{turn, Executing}
+	//Notify io that we are going to begin executing the Game of Life
+	c.events <- StateChange{0, Executing}
+
+	//Dial the server with the IP and port of our AWS server
+	//TODO MAKE SURE THE SERVER ADDRESS IS CORRECT AS IT CHANGES WHEN YOU INITIALISE A NEW SERVER
 	fmt.Println("Dialling")
-	client, err := rpc.Dial("tcp", "3.84.28.129:8030")
+	client, err := rpc.Dial("tcp", ":8030")
 	if err != nil {
 		log.Fatalf("Error connecting to serer: %v", err)
 	}
@@ -69,19 +91,23 @@ func distributor(p Params, c distributorChannels) {
 		}
 	}(client)
 
+	//Initialise our response struct
 	var response Response
 
+	// Ticker to report number of cells alive every 2 seconds using an RPC call
 	fmt.Println("Ticker function")
-	// ticker to report number of cells alive every 2 seconds
 	go func() {
 		ticker := time.NewTicker(2 * time.Second)
 		for {
 			select {
+			//Use quit channel to safely stop the goroutine and ticker if need be
 			case <-quit:
 				ticker.Stop()
 				fmt.Println("Ticker Quitting")
 				return
 			case <-ticker.C:
+				//Initialise a new response struct, then use an RPC request to get the required information from the server
+				//Then send an AliveCellsCount event to io using this information
 				var aliveCellsResponse AliveCellsResponse
 				fmt.Println("Requesting Alive Cells")
 				err = client.Call("ParamService.AliveCellsEvent", request, &aliveCellsResponse)
@@ -89,10 +115,11 @@ func distributor(p Params, c distributorChannels) {
 					log.Fatalf("RPC error: %v", err)
 				}
 				c.events <- AliveCellsCount{aliveCellsResponse.TurnsElapsed, aliveCellsResponse.NumAliveCells}
-
 			}
 		}
 	}()
+
+	//Use an RPC call to start running the game simulation on the server
 	fmt.Println("Running GameSim")
 	err = client.Call("ParamService.GameSimulation", request, &response)
 
@@ -101,33 +128,54 @@ func distributor(p Params, c distributorChannels) {
 		log.Fatalf("RPC error: %v", err)
 	}
 	fmt.Println("GameSim done")
-	// TODO: Report the final state using FinalTurnCompleteEvent.
+
+	//Create a slice of alive cells using the response, and get the number of turns that had elapsed in the simulation
 	alive := response.AliveCells
 	turnsElapsed := response.TurnsElapsed
 
-	c.ioCommand <- ioOutput
-	c.ioFilename <- (strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight) + "x" + strconv.Itoa(response.TurnsElapsed))
-	fmt.Println("Outputting World")
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			c.ioOutput <- response.FinalWorld[y][x]
-		}
-	}
+	//Output the .pgm file with the final state of the world
+	outputPgm(c, response.FinalWorld, p, turnsElapsed)
 
-
+	//Now that every turn has been processed, quit the ticker goroutine safely and make sure io is idle
 	quit <- true
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
-	c.events <- FinalTurnComplete{turnsElapsed, alive} //Uses FinalTurnComplete with calculateAliveCells
+	//Notify io that the final turn has been completed
+	c.events <- FinalTurnComplete{turnsElapsed, alive}
 
 
 	// Make sure that the Io has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
-	c.events <- StateChange{turn, Quitting}
+	//Notify io that the program is quitting
+	c.events <- StateChange{turnsElapsed, Quitting}
 	fmt.Println("Done\n\n")
+
 	// Close the channel to stop the SDL goroutine gracefully. Removing may cause deadlock.
 	close(c.events)
+}
+
+//outputPgm is used whenever we want to output a given board state as a .pgm file
+func outputPgm(c distributorChannels, world [][]byte, p Params, turn int) {
+
+	//Give io the command to output a .pgm file and give it the correct filename based on the baord dimensions and how many turns have been completed at this point
+	filename := strconv.Itoa(p.ImageWidth) + "x" + strconv.Itoa(p.ImageHeight) + "x" + strconv.Itoa(turn)
+	c.ioCommand <- ioOutput
+	c.ioFilename <- filename
+
+	//Iterate through all the cells and send them down the ioOutput channel for io to use to output the .pgm file
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			c.ioOutput <- world[y][x]
+		}
+	}
+
+	//Make sure io is idle before doing anything else
+	c.ioCommand <- ioCheckIdle
+	<-c.ioIdle
+
+	//Once io is idle and we know it isn't computing anything, notify io that the image output has been completed using the ImageOutputComplete event
+	c.events <- ImageOutputComplete{turn, filename}
 }
--- server/server.go
+++ server/server.go
@@ -29,6 +29,8 @@ type Response struct {
 	TurnsElapsed int         // Exported field
 }
 
+//AliveCellsResponse holds response data for whenever we want to report the number of alive cells
+//Which in our case is done using a ticker on the client side
 type AliveCellsResponse struct {
 	NumAliveCells int
 	TurnsElapsed int
@@ -37,6 +39,9 @@ type AliveCellsResponse struct {
 // ParamService is the struct that defines the RPC methods (exported)
 type ParamService struct{}
 
+//Declare the global variables world and turn so that we can report number of alive cells
+//Since otherwise there is no way for AliveCellsEvent to access world or turn
+//Also declare our mutex to help prevent race conditions
 var world [][]byte
 var turn int
 var mutex sync.Mutex
@@ -44,20 +49,21 @@ var mutex sync.Mutex
 // GameSimulation is the RPC method that performs the Game of Life simulation
 func (ps *ParamService) GameSimulation(request *GolRequest, reply *Response) error { // Changed to exported type
 
+	//Initialise world to be the same as in the request, as well as turn and setting p as the request params to make life easier
 	p := request.Params
-
     initialiseWorld(p)
-
 	world = request.World
 	turn = 0
 
-	// Execute all turns of the Game of Life
+	//Lock the mutex and calculate the next state of the world, then set it to be world
+	//Increment the turn counter before unlocking the mutex
+	//This repeats for the number of turns specified in Params
 	if p.Turns > 0 {
 		for i := 0; i < p.Turns; i++ {
 			mutex.Lock()
 			fmt.Printf("Executing turn %d\n", i)
-			world = calculateNextState(p, world) // Iterate through all turns
-			turn++                               // Increment the turn counter
+			world = calculateNextState(p, world)
+			turn++
 			mutex.Unlock()
 		}
 	}
@@ -74,19 +80,21 @@ func (ps *ParamService) GameSimulation(request *GolRequest, reply *Response) err
 
 // calculateNextState calculates the next state of the world based on the current state
 func calculateNextState(p Params, world [][]byte) [][]byte {
-	var alive []util.Cell                  // Make a slice of alive cells
-	newWorld := make([][]byte, len(world)) // Create a new world to return
-	copy(newWorld, world)                  // Copy current world to newWorld
+	//Make a slice of alive cells, and initialise a new world to return
+	var alive []util.Cell
+	newWorld := make([][]byte, len(world))
+	copy(newWorld, world)
 
-	for y := 0; y < p.ImageHeight; y++ { // Iterate through all rows
-		for x := 0; x < p.ImageWidth; x++ { // Iterate through all columns
-			aliveNeighbours := aliveNeighbours(p, world, x, y) // Count alive neighbours
+	//Iterate through all cells and count how many alive neighbours they have
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			aliveNeighbours := aliveNeighbours(p, world, x, y)
 
-			// Apply Game of Life rules
+			// Apply Game of Life rules. If a cell should be alive next turn, add it to the slice of alive cells
 			if world[y][x] == 255 && (aliveNeighbours == 2 || aliveNeighbours == 3) {
-				alive = append(alive, util.Cell{X: x, Y: y}) // Cell remains alive
+				alive = append(alive, util.Cell{X: x, Y: y})
 			} else if world[y][x] == 0 && aliveNeighbours == 3 {
-				alive = append(alive, util.Cell{X: x, Y: y}) // Cell becomes alive
+				alive = append(alive, util.Cell{X: x, Y: y})
 			}
 		}
 	}
@@ -100,43 +108,54 @@ func calculateNextState(p Params, world [][]byte) [][]byte {
 
 	// Update newWorld with alive cells
 	for _, aliveCell := range alive {
-		newWorld[aliveCell.Y][aliveCell.X] = 255 // Set alive cells to 255
+		newWorld[aliveCell.Y][aliveCell.X] = 255
 	}
 
+	//Return newWorld
 	return newWorld
 }
 
 // aliveNeighbours counts the alive neighbours of a given cell
 func aliveNeighbours(p Params, world [][]byte, x int, y int) int {
+	//Initialise the sum of alive neighbours as 0
 	sum := 0
 
-	// Check all 8 possible neighbours
+	//Iterate through the 8 cells surrounding the inputed cell, starting with the top left, then going towards top rmiddle, top right, middle left etc
+	//When i and j are 0, it means it is checking if itself is alive or dead which we don't want, so we use continue
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
-			if i == 0 && j == 0 { // Skip the current cell
+			if i == 0 && j == 0 {
 				continue
 			}
+			//The ny and nx are defined in ways so that if the surrounding cell is "off" the board (outside the height/width in params), it wraps around to the other side of the board
+			//After that, if the surrounding cell is alive, we increment the sum of alive neighbours.
 			ny := (y + i + p.ImageHeight) % p.ImageHeight
 			nx := (x + j + p.ImageWidth) % p.ImageWidth
 			if world[ny][nx] == 255 {
-				sum++ // Count alive neighbours
+				sum++
 			}
 		}
 	}
-
-	return sum // Return total alive neighbours
+	//After going through all surrounding cells, return the number of alive neighbours
+	return sum
 }
 
 // calculateAliveCells returns a list of alive cells in the world
 func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+
+	//Create a slice for storing the alive cells
 	var alive []util.Cell
+
+	//Iterate through every cell. If it's value is 255 (alive), add it to the slice of alive cells
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			if world[y][x] == 255 {
-				alive = append(alive, util.Cell{X: x, Y: y}) // Collect alive cells
+				alive = append(alive, util.Cell{X: x, Y: y})
 			}
 		}
 	}
+
+	//Finally, return the slice of alive cells
 	return alive
 }
 
@@ -147,24 +166,32 @@ func handleError(err error) {
 	}
 }
 
-func (ps *ParamService) AliveCellsEvent(request *GolRequest, reply *AliveCellsResponse) error {
+//Function to handle the response when we request the number of alive cells
+func (ps *ParamService) AliveCellsEvent(request *GolRequest,reply *AliveCellsResponse) error {
+
+	//Lock the mutex to avoid race conditions and initialise the number of alive cells
 	mutex.Lock()
-	var alive []util.Cell
-	for y := range(world) {
-		for x := range(world[y]) {
+	sum := 0
+
+	//Iterate through every cell and add all alive cells to the slice of alive cells
+	for y := 0; y < request.Params.ImageHeight; y++ {
+		for x := 0; x < request.Params.ImageWidth; x++ {
 			if world[y][x] == 255 {
-				alive = append(alive, util.Cell{X: x, Y: y}) // Collect alive cells
+				sum++
 			}
 		}
 	}
 
-	reply.NumAliveCells = len(alive)
+	//Populate the response with the number of alive cells and number of turns elapsed
+	reply.NumAliveCells = sum
 	reply.TurnsElapsed = turn
 
+	//Unlock the mutex
 	mutex.Unlock()
 	return nil
 }
 
+//Initialise the world correctly to avoid errors
 func initialiseWorld(p Params) {
 		fmt.Println("Initialising World")
 		world = make([][]byte, p.ImageHeight)
@@ -182,12 +209,14 @@ func initialiseWorld(p Params) {
 
 // main starts the RPC server
 func main() {
-	paramService := new(ParamService) // Create a new ParamService instance
+	//Create a new ParamService instance and register it
+	paramService := new(ParamService)
 	err := rpc.Register(paramService)
 	if err != nil {
 		return
-	} // Register the service
+	}
 
+	//Listen on port :8030 and ensure the listener is closed on exit
 	pAddr := flag.String("port", "8030", "Port to listen on")
 	flag.Parse()
 	ln, err := net.Listen("tcp", ":"+*pAddr) // Listen on port 8030
@@ -197,16 +226,17 @@ func main() {
 		if err != nil {
 
 		}
-	}(ln) // Ensure the listener is closed on exit
+	}(ln)
 	fmt.Println("Listening on :8030")
 
 
 	for {
-		conn, err := ln.Accept() // Accept incoming connections
+		// Accept incoming connections and  serve each connection concurrently
+		conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println("Error accepting:", err.Error())
 			continue
 		}
-		go rpc.ServeConn(conn) // Serve each connection concurrently
+		go rpc.ServeConn(conn)
 	}
 }
